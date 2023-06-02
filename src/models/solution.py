6b155ba0c698430f310ff2a5c6e80b2c7c774573
Timothe Watteau <timothe.wt@gmail.com>
2023-06-02 14:21:29 +0200

    created solution class and prepared functions

---
+++ src/models/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+	assigments: list[int]
+
+	def __init__(self, size: int):
+		assigments = [0] * size
+
+
+	def get_fitness_1(self) -> float:
+		return 0
+
+
+	def get_fitness_2(self) -> float:
+		return 0
+
+
+	def get_fitness_3(self) -> float:
+		return 0
+
+
+	def mutate(self): None:
+		pass
+
+
+	def __str__(self):
+		return f"{self.assigments}"
+
+
+	def __repr__(self):
+		return self.__str__()
+
