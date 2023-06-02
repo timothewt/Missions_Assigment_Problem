c907add6f66944f2fb787916e5948dae8f812064
Elise Albrecht <vitally.tally67@gmail.com>
2023-06-02 16:02:44 +0200

    fitness_1 ok, first draft fitness_2

--- src/models/solution.py
+++ src/models/solution.py
@@ -1,18 +1,35 @@
 class Solution:
-	assigments: list[int]
+	assignments: list[int]
 	
 
 	def __init__(self, size: int):
-		assigments = [0] * size
+		self.assignments = [0] * size
 
 
 	def get_fitness_1(self) -> float:
+		count = len(self.assignments)
+		for n in self.assignments:
+			if n == 0:
+				count -= 1
+		return count
+
+
+	def get_fitness_2(self, distance_matrix: list[list[float]], mission_matrix : list[list[int]]) -> float:
+
+
 		return 0
 
+	def compute_E(self, nb_employee: int, nb_mission : int) -> list[list[int]]:
+		E = [[0] * nb_mission for _ in range(nb_employee)]  # Matrice vide pour stocker les assignations des intervenants
+		for employee in range(1, nb_employee+1):
+			for n in self.assignments:
+				if n == employee:
+					check_schedule
+					insertion
+		return E
 
-	def get_fitness_2(self, distance_matrix: list[list[float]]) -> float:
-		return 0
-
+	def check_schedule(self, E: list[list[int]], M: ):
+		for
 
 	def get_fitness_3(self) -> float:
 		return 0
@@ -21,9 +38,11 @@ class Solution:
 	def mutate(self) -> None:
 		pass
 
+	def evaluate(self) -> list[float]:
+		return 0
 
 	def __str__(self):
-		return f"{self.assigments}"
+		return f"{self.assignments}"
 
 
 	def __repr__(self):
